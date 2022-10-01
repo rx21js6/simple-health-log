3350e1783c46968803c6550d23d126089d973385
developer <developer@dev-ws-win>
2022-10-01 15:40:43 +0900

    テーブル作成

---
+++ src/main/resources/db/migration/V1_2_2__insert_not_entered_notice.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (1, 'AWAKE_TIME', 'contents.record.dailyRecord.label.awakeTime', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (2, 'BED_TIME', 'contents.record.dailyRecord.label.bedTime', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (3, 'TEMP_MORNING', 'contents.record.dailyRecord.label.tempMorning', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (4, 'TEMP_EVENING', 'contents.record.dailyRecord.label.tempEvening', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (5, 'OX_SAT_MORNING', 'contents.record.dailyRecord.label.oxSatMorning', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO public.not_entered_notice(
+    id, type_key, message_id, checked, created_by, created_date, modified_by, modified_date)
+    VALUES (6, 'OX_SAT_EVENING', 'contents.record.dailyRecord.label.oxSatEvening', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
